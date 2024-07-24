6cc8d1d3a3cc04f3720c68547a24d617064398de
_Kentoyyy <kenalcantara55@gmail.com>
2024-07-24 18:26:13 +0800

    ups

--- src/assets/Components/Intro/intro.jsx
+++ src/assets/Components/Intro/intro.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import './intro.css'
 import profile from '../../Images/kprofile.png'
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Intro = () => {
   return (
     <section id='intro'>
       <div className="introContent">
-        <span className="hello">Hello,</span>
-        <span className="introText">I'm <span className="introName">Ken</span><br />Font end Developer</span>
-        <p className="introPara">I am a skilled </p>
+        <span className="hello">Hi there!, I'm </span>
+        <span className="introText"><span className="introName">Ken Alcantara</span></span>
+        <p className="introPara">A Front-End Developer specialized in mobile development<br></br>with an interest in web development.</p>
 
 
       </div>
-      <img src={profile} alt='Logo' className='logo' />
+      <img src={profile} alt='Profile' className='profile' />
     </section>
   );
 }
