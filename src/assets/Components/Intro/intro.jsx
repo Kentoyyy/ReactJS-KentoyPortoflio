252eb904271b35e8d68311beb3b7cbf057ae3175
_Kentoyyy <kenalcantara55@gmail.com>
2024-07-24 14:47:42 +0800

    introprofile

--- src/assets/Components/Intro/intro.jsx
+++ src/assets/Components/Intro/intro.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import './intro.css'
-import profile from '../../Images/profile.png'
+import profile from '../../Images/kprofile.png'
 
 const Intro = () => {
   return (
     <section id='intro'>
       <div className="introContent">
         <span className="hello">Hello,</span>
-        <span className="introText">I'm <span className="introname">Ken</span><br />Font end Developer</span>
+        <span className="introText">I'm <span className="introName">Ken</span><br />Font end Developer</span>
+        <p className="introPara">I am a skilled </p>
 
 
       </div>
+      <img src={profile} alt='Logo' className='logo' />
     </section>
   );
 }
