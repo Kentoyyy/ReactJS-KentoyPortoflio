895c1b89a176e8a09f8d195967be41839d9fc0d3
_Kentoyyy <kenalcantara55@gmail.com>
2024-07-24 12:56:19 +0800

    intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './assets/Components/Navbar/Navbar'
+import Intro from './assets/Components/Intro/intro'
 
 const App = () => {
   return (
    <div>
     <Navbar/>
+    <Intro/>
    </div>
   )
 }
--- src/assets/Components/Intro/intro.jsx
+++ src/assets/Components/Intro/intro.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import './intro.css'
+import profile from '../../Images/profile.png'
 
-const intro = () => {
+const Intro = () => {
   return (
-    <div>intro</div>
-  )
+    <section id='intro'>
+      <div className="introContent">
+        <span className="hello">Hello,</span>
+        <span className="introText">I'm <span className="introname">Ken</span><br />Font end Developer</span>
+
+
+      </div>
+    </section>
+  );
 }
 
-export default intro
+export default Intro 
