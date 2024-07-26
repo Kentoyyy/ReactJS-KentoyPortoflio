24c08a8c88a5a87fd72ce526c3d627428f65d895
_Kentoyyy <kenalcantara55@gmail.com>
2024-07-26 19:59:32 +0800

    uupp

--- src/assets/Components/Home/skills.jsx
+++ src/assets/Components/Home/skills.jsx
@@ -12,28 +12,28 @@ const MySkills = () => {
       </div>
       <div className="skillsContainer">
         <div className="myskills">
-          <BsWindow color='#fff' size={32} className="skillIcon" />
+          <BsWindow color='#fff' size={24} className="skillIcon" />
           <div className='myskillText'>
             <h2>WEBSITES</h2>
             <p>Lorem ipsum dolor sit amet. Et autem praesentium qui magnam ducimus vel sequi reprehenderit. Aut sint quos qui provident quis eos pariatur ipsam ea fuga sequi. Et cumque sequi sit error repudiandae aut suscipit nesciunt est quia asperiores non excepturi eaque. Est rerum consequatur sed voluptas nobis qui nihil ipsam.</p>
           </div>
         </div>
         <div className="myskills">
-          <BsGlobe color='#fff' size={32} className="skillIcon" />
+          <BsGlobe color='#fff' size={24} className="skillIcon" />
           <div className='myskillText'>
             <h2>WEB APPS</h2>
             <p>Lorem ipsum dolor sit amet. Et autem praesentium qui magnam ducimus vel sequi reprehenderit. Aut sint quos qui provident quis eos pariatur ipsam ea fuga sequi. Et cumque sequi sit error repudiandae aut suscipit nesciunt est quia asperiores non excepturi eaque. Est rerum consequatur sed voluptas nobis qui nihil ipsam.</p>
           </div>
         </div>
         <div className="myskills">
-          <BsPhone color='#fff' size={32} className="skillIcon" />
+          <BsPhone color='#fff' size={24} className="skillIcon" />
           <div className='myskillText'>
             <h2>MOBILE APPS</h2>
             <p>Lorem ipsum dolor sit amet. Et autem praesentium qui magnam ducimus vel sequi reprehenderit. Aut sint quos qui provident quis eos pariatur ipsam ea fuga sequi. Et cumque sequi sit error repudiandae aut suscipit nesciunt est quia asperiores non excepturi eaque. Est rerum consequatur sed voluptas nobis qui nihil ipsam.</p>
           </div>
         </div>
         <div className="myskills">
-          <FaShareAlt color='#fff' size={32} className="skillIcon" />
+          <FaShareAlt color='#fff' size={24} className="skillIcon" />
           <div className='myskillText'>
             <h2>ML/AI</h2>
             <p>Lorem ipsum dolor sit amet. Et autem praesentium qui magnam ducimus vel sequi reprehenderit. Aut sint quos qui provident quis eos pariatur ipsam ea fuga sequi. Et cumque sequi sit error repudiandae aut suscipit nesciunt est quia asperiores non excepturi eaque. Est rerum consequatur sed voluptas nobis qui nihil ipsam.</p>
