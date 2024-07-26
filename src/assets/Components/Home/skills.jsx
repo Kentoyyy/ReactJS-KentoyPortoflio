bff209a32e17db6baf2b37b6367bdefd8777ad8d
_Kentoyyy <kenalcantara55@gmail.com>
2024-07-26 17:44:21 +0800

    skills

--- src/assets/Components/Home/skills.jsx
+++ src/assets/Components/Home/skills.jsx
@@ -4,26 +4,27 @@ import './../Style/skills.css'
 
 const techstack = () => {
   return (
+
     <section id='skills'>
-      <span className="skillTitles">What i can do?</span>
+      <span className="skillTitles">What I Can Do?</span>
       <span className="skillDescription">Do I need to define and explain it? You can see my skills in my recent work.</span>
       <div className='skillList'>
         <div className="myskills">
-          <BsWindow size={52} className="skillWindow" />
+          <BsWindow color='#fff' size={52} className="skillWindow" />
           <div className='myskillText'>
             <h2>WEBSITES</h2>
             <p>I can assist with the front-end development of your website to ensure it looks great and functions smoothly.</p>
           </div>
         </div>
         <div className="myskills">
-          <BsGlobe size={52} className="skillWeb" />
+          <BsGlobe color='#fff' size={52} className="skillWeb" />
           <div className='myskillText'>
             <h2>WEB APPS</h2>
             <p>I can help with the front-end development of your website, making sure it’s both visually appealing and functional.</p>
           </div>
         </div>
         <div className="myskills">
-          <BsPhone size={52} className="skillPhone" />
+          <BsPhone color='#fff' size={52} className="skillPhone" />
           <div className='myskillText'>
             <h2>MOBILE APPS</h2>
             <p>f your target audience is mobile phone users or if you need a basic app for your business, I can provide a cross-platform solution. 📱</p>
