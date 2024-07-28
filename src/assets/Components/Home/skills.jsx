b2b524ca3df0aeff2e26749e395841e669e00107
_Kentoyyy <kenalcantara55@gmail.com>
2024-07-28 15:09:45 +0800

    up

--- src/assets/Components/Home/skills.jsx
+++ src/assets/Components/Home/skills.jsx
@@ -15,28 +15,28 @@ const MySkills = () => {
           <BsWindow color='#fff' size={24} className="skillIcon" />
           <div className='myskillText'>
             <h2>WEBSITES</h2>
-            <p>Lorem ipsum dolor sit amet. Et autem praesentium qui magnam ducimus vel sequi reprehenderit. Aut sint quos qui provident quis eos pariatur ipsam ea fuga sequi. Et cumque sequi sit error repudiandae aut suscipit nesciunt est quia asperiores non excepturi eaque. Est rerum consequatur sed voluptas nobis qui nihil ipsam.</p>
+            <p>Lorem ipsum dolor sit amet. Et autem praesentium qui magnam ducimus vel sequi reprehenderit. Aut sint quos qui provident quis eos pariatur ipsam ea fuga sequi. Et cumque sequi sit error repudiandae aut suscipit</p>
           </div>
         </div>
         <div className="myskills">
           <BsGlobe color='#fff' size={24} className="skillIcon" />
           <div className='myskillText'>
             <h2>WEB APPS</h2>
-            <p>Lorem ipsum dolor sit amet. Et autem praesentium qui magnam ducimus vel sequi reprehenderit. Aut sint quos qui provident quis eos pariatur ipsam ea fuga sequi. Et cumque sequi sit error repudiandae aut suscipit nesciunt est quia asperiores non excepturi eaque. Est rerum consequatur sed voluptas nobis qui nihil ipsam.</p>
+            <p>Lorem ipsum dolor sit amet. Et autem praesentium qui magnam ducimus vel sequi reprehenderit. Aut sint quos qui provident quis eos pariatur ipsam ea fuga sequi. Et cumque sequi sit error repudiandae aut suscipit</p>
           </div>
         </div>
         <div className="myskills">
           <BsPhone color='#fff' size={24} className="skillIcon" />
           <div className='myskillText'>
             <h2>MOBILE APPS</h2>
-            <p>Lorem ipsum dolor sit amet. Et autem praesentium qui magnam ducimus vel sequi reprehenderit. Aut sint quos qui provident quis eos pariatur ipsam ea fuga sequi. Et cumque sequi sit error repudiandae aut suscipit nesciunt est quia asperiores non excepturi eaque. Est rerum consequatur sed voluptas nobis qui nihil ipsam.</p>
+            <p>Lorem ipsum dolor sit amet. Et autem praesentium qui magnam ducimus vel sequi reprehenderit. Aut sint quos qui provident quis eos pariatur ipsam ea fuga sequi. Et cumque sequi sit error repudiandae aut suscipit</p>
           </div>
         </div>
         <div className="myskills">
           <FaShareAlt color='#fff' size={24} className="skillIcon" />
           <div className='myskillText'>
             <h2>ML/AI</h2>
-            <p>Lorem ipsum dolor sit amet. Et autem praesentium qui magnam ducimus vel sequi reprehenderit. Aut sint quos qui provident quis eos pariatur ipsam ea fuga sequi. Et cumque sequi sit error repudiandae aut suscipit nesciunt est quia asperiores non excepturi eaque. Est rerum consequatur sed voluptas nobis qui nihil ipsam.</p>
+            <p>Lorem ipsum dolor sit amet. Et autem praesentium qui magnam ducimus vel sequi reprehenderit. Aut sint quos qui provident quis eos pariatur ipsam ea fuga sequi. Et cumque sequi sit error repudiandae aut suscipit</p>
           </div>
         </div>
       </div>
--- src/assets/Components/Navbar/Navbar.jsx
+++ src/assets/Components/Navbar/Navbar.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
-import './../Style/Navbar.css'
-import cvlogo from '../../Images/downloadcv.png'
+import React, { useState } from 'react';
+import './../Style/Navbar.css';
+import cvlogo from '../../Images/downloadcv.png';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className='header'>
-         <a href='/' className='resume'>  
+      <a href='/' className='resume'>  
         <img src={cvlogo} alt='Logo' className='logo' />
         Resume/CV
       </a>
 
-        <nav className='navbar'>
-            <a href='/'>Home</a>
-            <a href='/'>Techstack</a>
-            <a href='/'>Works</a>
-            <a href='/'>Certificates</a>
-            <a href='/'>About Me</a>
-            <a href='/'>Contact</a>
-        </nav>
-    </header>
-  )
-}
+      <div className='menu-icon' onClick={toggleMenu}>
+        &#9776;
+      </div>
 
-export default Navbar
+      <nav className={`navbar ${menuOpen ? 'open' : ''}`}>
+        <a href='/'>Home</a>
+        <a href='/'>Techstack</a>
+        <a href='/'>Works</a>
+        <a href='/'>Certificates</a>
+        <a href='/'>About Me</a>
+        <a href='/'>Contact</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
