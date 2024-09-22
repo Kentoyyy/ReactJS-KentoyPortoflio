cf50d1f085e4064c4f790e5b70d9d33d47ea3352
_Kentoyyy <69066165+Kentoyyy@users.noreply.github.com>
2024-09-22 22:23:38 +0800

    Update about.jsx

--- src/assets/Components/Home/about.jsx
+++ src/assets/Components/Home/about.jsx
@@ -10,7 +10,7 @@ const About = () => {
           I'm Ken Alcantara a <span className="highlight"> a front-end developer specializing in web and full-stack mobile developer</span>. I'm currently taking a bachelor degree in <span className="highlight">Computer Science</span> with specialization in <span className="highlight">Machine Learning</span> at <span className="highlight">Cavite State University - Bacoor Campus</span>. Although my degree focuses on machine learning and artificial intelligence, my passion lies in <span className="highlight">mobile development, with a keen interest in web development.</span>.
         </p>
         <p className="aboutDesc">
-        I began my programming journey during my Senior High School years at<span className="highlight">STI College - Lipa</span> taking <span className="highlight">Information, Communication and Technology strand</span>. I was taught Web Development (HTML, CSS, JavaScript) and Native Mobile App Development (Java - Android Studio). 
+        I began my programming journey during my Senior High School year at<span className="highlight">STI College - Lipa</span> taking <span className="highlight">Information, Communication and Technology strand</span>. I was taught Web Development (HTML, CSS, JavaScript) and Native Mobile App Development (Java - Android Studio). 
         </p>
       </div>
     </section>  
